7ee3fbaf0fe960a2bb475c9731eb33a0a15457ca
Felipe Batista E Baptista <38279457801@info.local>
2023-03-30 14:17:03 -0300

    subir projeto

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.WriteLine(@$"
+-----------------
+| Programa para |
+| calcular IMC  |
+-----------------
+");
+Console.ResetColor();
+
+Console.BackgroundColor = ConsoleColor.Green;
+
+Console.WriteLine($"Informe o nome do paciente: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Informe o peso atual do paciente: ");
+float peso = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe a altura do paciente: ");
+float altura = float.Parse(Console.ReadLine());
+
+Console.ResetColor();
+
+float imc = peso / ((float)Math.Pow(altura,2));
+
+Console.WriteLine(imc);
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+
+Console.WriteLine($"O paciente (nome) tem o imc igual a (imc)");
+Console.ResetColor();
+
+
+
